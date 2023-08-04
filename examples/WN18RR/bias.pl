052feeaa31a2bc8fceba07dcd74e80deae13a878
trungle <le.hg.trung@gmail.com>
2023-08-04 22:35:46 +0700

    add wn188rr

--- examples/WN18RR/bias.pl
+++ examples/WN18RR/bias.pl
@@ -11,3 +11,13 @@ body_pred(member_of_domain_usage,2).
 body_pred(member_of_domain_region,2).
 body_pred(verb_group,2).
 body_pred(similar_to,2).
+:- discontiguous instance_hypernym/2.
+:- discontiguous hypernym/2.
+:- discontiguous also_see/2.
+:- discontiguous member_meronym/2.
+:- discontiguous synset_domain_topic_of/2.
+:- discontiguous has_part/2.
+:- discontiguous member_of_domain_usage/2.
+:- discontiguous member_of_domain_region/2.
+:- discontiguous verb_group/2.
+:- discontiguous similar_to/2.
